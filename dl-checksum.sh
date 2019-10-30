b2c200c5fe569c1b65cc5e11eb12006adfd48e26
Andrew Rothstein <andrew.rothstein@gmail.com>
2019-10-29 21:54:25 -0500

    tar.gz -> zip for windows distros v0.10.0+

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -1,24 +1,25 @@
 #!/usr/bin/env sh
-VER=${1:-v0.9.8}
+VER=${1:-v0.10.0}
 DIR=~/Downloads
 MIRROR=https://github.com/liquidata-inc/dolt/releases/download/${VER}
 
 dl()
 {
-    OS=$1
-    ARCH=$2
-    PLATFORM=${OS}-${ARCH}
-    RFILE=dolt-${PLATFORM}.tar.gz
-    URL=$MIRROR/$RFILE
-    LFILE=$DIR/dolt-${PLATFORM}-${VER}.tar.gz
+    local os=$1
+    local arch=$2
+    local archive_type=${3:-tar.gz}
+    local platform=${os}-${arch}
+    local rfile=dolt-${platform}.${archive_type}
+    local url=$MIRROR/$rfile
+    local lfile=$DIR/dolt-${platform}-${VER}.tar.gz
 
-    if [ ! -e $LFILE ];
+    if [ ! -e $lfile ];
     then
-        wget -q -O $LFILE $URL
+        wget -q -O $lfile $url
     fi
 
-    printf "    # %s\n" $URL
-    printf "    %s: sha256:%s\n" $PLATFORM `sha256sum $LFILE | awk '{print $1}'`
+    printf "    # %s\n" $url
+    printf "    %s: sha256:%s\n" $platform `sha256sum $lfile | awk '{print $1}'`
 }
 
 printf "  %s:\n" $VER
@@ -26,8 +27,8 @@ dl darwin 386
 dl darwin amd64
 dl linux 386
 dl linux amd64
-dl windows 386
-dl windows amd64
+dl windows 386 zip
+dl windows amd64 zip
 
 
 
